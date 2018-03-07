33393699432ab8607766cc504c4cf3cc12208b72
Erik <erikwjonsson@gmail.com>
2018-03-07 17:48:19 +0100

    Removed pry statement.

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,7 +2,6 @@ class Lead < ApplicationRecord
 	has_one :qualified_leads
 
 	def self.convert_to_qualified_data(lead_ids)
-		binding.pry
 		unless lead_ids.nil?
 			lead_ids.each do |lead_id|
 				QualifiedLead.create!(lead_id: lead_id)
